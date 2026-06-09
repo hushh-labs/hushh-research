8ff9df596d0f5930334098d7c0c5e3e387080cf8
Madana Rajith <rajithmadana@gmail.com>
2026-06-09 12:46:37 +0530

    fix: correct import placement for redact_log_value in one_location_agent_service
    Signed-off-by: Madana Rajith <rajithmadana@gmail.com>

--- consent-protocol/hushh_mcp/services/one_location_agent_service.py
+++ consent-protocol/hushh_mcp/services/one_location_agent_service.py
@@ -12,7 +12,6 @@ from typing import Any
 from api.utils.fcm_messages import build_push_message
 from api.utils.firebase_admin import ensure_firebase_admin
 from db.db_client import DatabaseExecutionError, get_db
-from mcp_modules.log_redaction import redact_log_value
 from hushh_mcp.operons.location.policy import (
     LOCATION_CAPABILITY_SCOPES,
     normalize_duration_hours,
