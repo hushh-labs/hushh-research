9bb30f0fa5d120f04b1ef879f37533bc89b4d018
Divya-rajendran009 <divya.rajendranps@gmail.com>
2026-06-19 19:24:12 +0530

    test(use-toggle): cover initial false state
    Signed-off-by: Divya-rajendran009 <divya.rajendranps@gmail.com>

---
+++ hushh-webapp/hooks/use-toggle.ts
@@ -0,0 +1,21 @@
+"use client";
+
+import { useCallback, useState } from "react";
+
+export function useToggle(initialValue = false) {
+  const [value, setValue] = useState(initialValue);
+
+  const toggle = useCallback(() => {
+    setValue((current) => !current);
+  }, []);
+
+  const setOn = useCallback(() => {
+    setValue(true);
+  }, []);
+
+  const setOff = useCallback(() => {
+    setValue(false);
+  }, []);
+
+  return { value, toggle, setOn, setOff };
+}
