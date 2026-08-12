eb710a2138a8f728929ccf08fa0e852dc1e9d053
Roopmann30 <roopmannvaradharaj@gmail.com>
2026-08-12 20:09:14 +0530

    fix(ui): remove excessive bottom padding on connect page
    Signed-off-by: Roopmann30 <roopmannvaradharaj@gmail.com>

--- hushh-webapp/app/connect/page-client.tsx
+++ hushh-webapp/app/connect/page-client.tsx
@@ -1113,9 +1113,8 @@ export default function ConnectPageClient() {
     <AppPageShell
       as="main"
       fitContent
-
       width="reading"
-      className="relative isolate pb-[calc(var(--app-bottom-fixed-ui,96px)+1.25rem)] sm:pb-10 md:pb-8"
+      className="relative isolate"
       nativeTest={{
         routeId: "/one/connect",
         marker: "native-route-connect",
